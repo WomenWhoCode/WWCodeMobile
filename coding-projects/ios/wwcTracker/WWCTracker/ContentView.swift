45a2f51e1784a774ae8a45902a8abefa75af3d52
Claudia Maciel <clc_80@yahoo.com>
2024-01-19 20:58:43 -0800

    Created iOS initial structure
    addresses issue [#68](https://github.com/WomenWhoCode/WWCodeMobile/issues/68)
    
    Created a new iOS project
    Created two groups one for the "Details Screen" and one for the "List Screen"
    Each group contains empty Model, View, and Model View  files

---
+++ coding-projects/ios/wwcTracker/WWCTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wwcTracker
+//
+//  Created by Claudia Maciel on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coding-projects/ios/wwcTracker/WWCTracker/WWCTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWCTrackerApp.swift
+//  wwcTracker
+//
+//  Created by Claudia Maciel on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WWCTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
